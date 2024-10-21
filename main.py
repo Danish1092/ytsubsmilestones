99fbb411859514defdde1c72c5c21cabc68416e5
Danish1092 <syeddanish1092@gmail.com>
2024-10-21 15:43:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import requests
 import json
 from io import BytesIO
 
-# Twitter API credentials
 bearer_token = os.environ['TWITTER_BEARER_TOKEN']
 consumer_key = os.environ['TWITTER_CONSUMER_KEY']
 consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
 access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-# V1 Twitter API Authentication for media uploads
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# V2 Twitter API Authentication for creating tweets
 client = tweepy.Client(
     bearer_token,
     consumer_key,
@@ -26,7 +23,6 @@ client = tweepy.Client(
     wait_on_rate_limit=True,
 )
 
-# YouTube Data API v3 Key
 API_KEY = os.environ['YOUTUBE_API_KEY']
 
 def fetch_channel_details(channel_id):
@@ -40,10 +36,8 @@ def fetch_channel_details(channel_id):
         if "items" in data and len(data["items"]) > 0:
             channel_info = data["items"][0]
 
-            # Extracting subscriber count
             subscriber_count = int(channel_info["statistics"]["subscriberCount"])
 
-            # Extracting profile picture URL
             profile_picture_url = channel_info["snippet"]["thumbnails"]["high"]["url"]
             return subscriber_count, profile_picture_url
         else:
@@ -55,30 +49,24 @@ def fetch_channel_details(channel_id):
         return None, None
 
 def check_and_tweet():
-    # Load the channel data from the JSON file
     with open('channel_data.json', 'r') as f:
         channels = json.load(f)
 
     tweets_sent = False
 
-    # Loop through each channel in the JSON file
     for channel in channels:
         current_count, profile_pic_url = fetch_channel_details(channel["id"])
         if current_count is not None:
-            # Calculate the next million milestone
             current_milestone = (current_count // 1000000) * 1000000
             previous_milestone = channel.get("last_tweeted_milestone", 0)
 
-            # Debugging: Checking the milestone calculation
             print(f"Checking {channel['name']} - Current Milestone: {current_milestone}, Previous Milestone: {previous_milestone}")
 
-            # Check if the current milestone is greater than the previous one
             if current_milestone > previous_milestone:
                 tweet_text = (f"{channel['name']} has crossed {current_milestone // 1000000} Million subscribers on YouTube!\n"
                               f"#{channel['name'].replace(' ', '')} #youtube")
 
                 try:
-                    # Tweet with profile picture if available
                     if profile_pic_url:
                         response = requests.get(profile_pic_url)
                         response.raise_for_status()
@@ -86,13 +74,11 @@ def check_and_tweet():
                         media_id = api.media_upload(filename="profile_pic.jpg", file=image_bytes).media_id_string
                         client.create_tweet(text=tweet_text, media_ids=[media_id])
                     else:
-                        # Tweet without image if no profile picture is available
                         client.create_tweet(text=tweet_text)
 
                     print(f"Tweet sent: {tweet_text}")
                     tweets_sent = True
 
-                    # Update the last tweeted milestone after a successful tweet
                     channel["last_tweeted_milestone"] = current_milestone
 
                 except requests.exceptions.RequestException as e:
@@ -103,11 +89,7 @@ def check_and_tweet():
                         print(f"API Code: {e.api_code}")
                     if e.response:
                         print(f"Response content: {e.response.text}")
-
-            # Always update the subscriber count to the latest one
             channel["subscribers"] = current_count
-
-    # Write the updated channel data back to the JSON file
     with open('channel_data.json', 'w') as f:
         json.dump(channels, f, indent=4)
 
